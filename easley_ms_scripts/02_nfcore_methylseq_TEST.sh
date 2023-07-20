a9c0c16c10b1fdf6ce0878b580d6985ba20f9c82
Avril Maellen Harder <amh0254@c20-login01.cm.cluster>
2023-07-20 11:27:18 -0500

    update

--- easley_ms_scripts/02_nfcore_methylseq_TEST.sh
+++ easley_ms_scripts/02_nfcore_methylseq_TEST.sh
@@ -46,14 +46,25 @@ cp /home/amh0254/krat_roh_analyses/sample_lists/krat_ms_samplesheet_TEST.csv ./d
 
 mkdir results
 
-## run bismark test (works)
+## run bismark test (can't find .fa, even after it's indexed)
+# nextflow run nf-core/methylseq \
+# --input data/krat_ms_samplesheet_TEST.csv \
+# --outdir results \
+# --max_cpus 2 \
+# --fasta /scratch/avrilh/kratroh_01_assembindex/dspec_genbank_assem.fa \
+# --save_reference \
+# -resume \
+# -profile singularity
+
+## pointing directly to bismark index
 nextflow run nf-core/methylseq \
---input data/krat_ms_samplesheet.csv \
+--input data/krat_ms_samplesheet_TEST.csv \
 --outdir results \
 --max_cpus 2 \
---fasta /scratch/avrilh/kratroh_01_assembindex/dspec_genbank_assem.fa \
+--fasta /scratch/avrilh/ms_kratroh_02_nfcore_methylseq_TEST/bismark/work/a5/67d6e87d3641b4a45bbe62f9803f65/BismarkIndex/dspec_genbank_assem.fa \
+--bismark_index /scratch/avrilh/ms_kratroh_02_nfcore_methylseq_TEST/bismark/work/a5/67d6e87d3641b4a45bbe62f9803f65/BismarkIndex \
 --save_reference \
---resume \
+-resume \
 -profile singularity
 
 
