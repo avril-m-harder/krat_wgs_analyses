85ca3399c2d3ce781b5443118074a08fcd2f3eef
Avril Maellen Harder <amh0254@c20-login01.cm.cluster>
2023-07-25 15:58:00 -0500

    update nextflow run process

--- easley_ms_scripts/02_nfcore_methylseq.sh
+++ easley_ms_scripts/02_nfcore_methylseq.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#SBATCH --job-name=methylseq
-#SBATCH --partition=jrw0107_std 
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -t 300:00:00
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=avrilharder@gmail.com
-
 ##  Set username
 USER=avrilh
 
@@ -20,6 +12,8 @@ PROJ1=bismark
 
 ## --------------------------------
 ## Activate conda environment and load modules
+## (For some reason, I get java-related errors if I rely on the config file for 
+## these steps)
 
 module load python/anaconda/3.10.9
 
@@ -37,23 +31,24 @@ mkdir /scratch/${USER}/${DIR}/${PROJ1}/
 cd /scratch/${USER}/${DIR}/${PROJ1}/
 
 mkdir data
-# cp /home/amh0254/krat_roh_analyses/sample_lists/krat_ms_samplesheet.csv ./data/
+cp /home/amh0254/krat_roh_analyses/sample_lists/krat_ms_samplesheet.csv ./data/
+cp /home/amh0254/krat_roh_analyses/scripts/files/easleyconfig_amh.conf .
 
 
 ## --------------------------------
 ## Run nf-score methylseq pipeline
 
-# mkdir results
+mkdir results
 
 ## aligning with bismark -- requires a manual fix after this step
-# nextflow run nf-core/methylseq \
-# --input data/krat_ms_samplesheet.csv \
-# --outdir results \
-# --max_cpus 8 \
-# --fasta /scratch/avrilh/kratroh_01_assembindex/dspec_genbank_assem.fa \
-# --save_reference \
-# -resume \
-# -profile singularity
+nextflow run nf-core/methylseq \
+--input data/krat_ms_samplesheet.csv \
+--outdir results \
+--fasta /scratch/avrilh/kratroh_01_assembindex/dspec_genbank_assem.fa \
+--save_reference \
+-resume \
+-c easleyconfig_amh.conf \
+-profile singularity
 
 ## for some reason, ref .fa can't be used when it's just a symbolic link? idk. this fixes
 ## it tho, alongside specif.
@@ -62,14 +57,15 @@ mkdir data
 # cp /scratch/avrilh/kratroh_01_assembindex/dspec_genbank_assem.fa .
 
 
-nextflow run nf-core/methylseq \
---input data/krat_ms_samplesheet.csv \
---outdir results \
---fasta /scratch/avrilh/kratroh_01_assembindex/dspec_genbank_assem.fa \
---save_reference \
--resume \
--c /home/amh0254/krat_roh_analyses/scripts/files/easleyconfig_amh.conf \
--profile singularity
+# nextflow run nf-core/methylseq \
+# --input data/krat_ms_samplesheet.csv \
+# --outdir results \
+# --fasta /scratch/avrilh/kratroh_01_assembindex/dspec_genbank_assem.fa \
+# --save_reference \
+# -resume \
+# -c easleyconfig_amh.conf \
+# -profile singularity
 
 
-conda deactivate
+## below should be covered by config file
+# conda deactivate
